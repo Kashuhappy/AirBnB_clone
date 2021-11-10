1bee2883da693283ea94ebbdf2ad12728916364f
Kashu_happy <84641586+Kashuhappy@users.noreply.github.com>
2021-11-10 20:18:12 +0000

    Change0

--- models/base_model.py
+++ models/base_model.py
@@ -8,17 +8,20 @@ from datetime import datetime
 
 class BaseModel:
     # Creating class BaseModel
-    id = {""}
+    id = str(uuid.uuid4())
+    
+    created_at = datetime.now()
+    updated_at = datetime.now()
 
-    def __str__(self, name, id, __dict__):
-        self.name = name
-        self.id = id
-        self.__dict__ = __dict__
+    def __str__(self):
+        return f'[{self.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
-        updated_at.append(self)
+        self.updated_at = datetime.now()
 
-    def to_dict(self, **kwags):
-        for key, value in kwags.items():
-            print(key)
-            print(value)
+    def to_dict(self):
+        _dict = self.__dict__
+        _dict['__class__ '] = self.__name__
+        _dict['created_at'] = _dict['created_at'].isoformat('%Y-%m-%dT%H:%M:%S.%f')
+        _dict['updated_at'] = _dict['updated_at'].isoformat('%Y-%m-%dT%H:%M:%S.%f')
+        return _dict
