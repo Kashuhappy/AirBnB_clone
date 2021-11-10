c44aa2911a909a8e519e9716e716dade2b2fc9bb
root <knashipa@gmail.com>
2021-11-10 17:56:14 +0000

    Model files

--- models/base_model.py
+++ models/base_model.py
@@ -1,25 +1,24 @@
+#!usr/bin/python3
+# This class BaseModel
+# Defines all common attributes and methods of other classes
+
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
+    # Creating class BaseModel
     id = {""}
-    
+
     def __str__(self, name, id, __dict__):
         self.name = name
         self.id = id
         self.__dict__ = __dict__
-    
-    def save(self): 
+
+    def save(self):
         updated_at.append(self)
 
     def to_dict(self, **kwags):
-        for key,value in kwags.items():
+        for key, value in kwags.items():
             print(key)
             print(value)
-            
-
-
-        
-
-
