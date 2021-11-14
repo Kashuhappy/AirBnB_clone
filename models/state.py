11fe7b41690ca91ae399f47343c16cd7db8c9a28
root <knashipa@gmail.com>
2021-11-14 20:29:28 +0000

    State module

---
+++ models/state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+State module
+Inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """
+    Public class attribute
+        name: empty string
+    """
+
+    name = ""
