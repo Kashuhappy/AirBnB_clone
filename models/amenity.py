14779145794a6909fca2d00cf1009eba969025c6
root <knashipa@gmail.com>
2021-11-14 20:37:07 +0000

    Amenity Module

---
+++ models/amenity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Amenity module
+Inherits from BaseModel
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    Public class attribute:
+        name: empty string
+    """
+    name = ""
